7856b4cf8d1ff6cd61c643d9e3c28d81e32fcfbf
unknown <https://github.com/pedrohrs23/controle-financeiro.git>
2023-04-04 21:47:02 -0300

    finalização do desenvolvimento 04.04

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import {View, Text, StyleSheet} from 'react-native'
-import React, {useState} from 'react'
+import { View, Text, StyleSheet, FlatList, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import axios from 'axios';
+
 
 const iaChatGpt = () => {
   const [data, setData] = useState([])
@@ -9,27 +11,91 @@ const iaChatGpt = () => {
 
   const handleSend = async () => {
     const prompt = textInput
-    const response = await axios.post(apiUrl, )
+    const response = await axios.post(apiUrl, {
+      prompt: prompt,
+      max_tokens: 1024,
+      temperature: 0.5,
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ${apiKey}'
+      }
+    });
+    const text = response.data.choices[0].text;
+    setData(...data, { type: 'user', 'text': textInput }, { type: 'bot', 'text': text });
+    setTextInput('');
   }
-    return (
-        <view style={styles.container}>
-        <Text style={styles.title}>Ai ChatBot</Text>
-        </view>
-    )
+  return (
+    <view style={styles.container}>
+      <Text style={styles.title}>AI ChatBot</Text>
+      <FlatList
+        data={data}
+        keyExtractor={(item, index) => index.toString()}
+        style={styles.body}
+        renderItem={({ item }) => (
+          <View style={{ flexDirection: 'row', padding: 10 }}>
+            <Text style={{ fontWeight: 'bold', color: item.type === 'user' ? 'green' : 'red' }}>{item.type === 'user' ? 'Ninza' : 'Bot'}</Text>
+            <Text style={styles.bot}>{item.text}</Text>
+          </View>
+      )}/>
+      <TextInput
+          style={styles.input}
+          value={textInput}
+          onChangeText={Text => setTextInput(text)}
+          placeholder= "Ask me anything"
+      />
+      <TouchableOpacity>
+        style={styles.button}
+        onPress={handleSend}
+
+        <text style={styles.buttonText}>Let's Go</text>
+      </TouchableOpacity>
+    </view>
+  )
 }
 
 export default iaChatGpt
 
 const styles = StyleSheet.create({
-    container: {
+  container: {
       flex: 1,
       backgroundColor: '#fffcc9',
       alignItems: 'center',
-    },
-    title: {
+  },
+  title: {
       fontSize: 28,
       fontWeight: 'bold',
       marginBottom: 20,
       marginTop: 70
-    }
-  });
+  },
+  body: {
+      backgroundColor: '#fffcc9',
+      width: '102%',
+      margin: 70
+  },
+  bot: {
+      fontSize: 16
+  },
+  input:  {
+      borderwidth: 1,
+      bordercolor: 'black',
+      width: '90%',
+      height: 60,
+      marginBottom: 10,
+      borderRadius:10
+  },
+  button: {
+      backgroundColor: 'yellow',
+      width: '90',
+      height: 60,
+      borderRadius:10,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: 10
+  },
+  buttonText: {
+      fontSize: 25,
+      fontWeight: 'bold',
+      color: 'blue'
+  }
+});
